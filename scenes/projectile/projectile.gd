9e3e7c59fa410fcf69c515295eecc5917d112230
Ross Ware <rosswaredev@gmail.com>
2023-06-10 18:02:27 +1000

    dynamic collision layers in battle

--- scenes/projectile/projectile.gd
+++ scenes/projectile/projectile.gd
@@ -16,3 +16,10 @@ func _process(delta):
 
 func _on_Area2D_area_entered(_area):
 	queue_free()
+
+
+func init(is_from_enemy: bool):
+	var collision_layer = Constants.ENEMY_PROJECTILE_LAYER if is_from_enemy else Constants.PLAYER_PROJECTILE_LAYER
+	var mask_layer = Constants.PLAYER_UNIT_LAYER if is_from_enemy else Constants.ENEMY_UNIT_LAYER
+	set_collision_layer_value(collision_layer, true)
+	set_collision_mask_value(mask_layer, true)
--- scenes/unit/unit.gd
+++ scenes/unit/unit.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Area2D
 class_name Unit
 
 
@@ -8,20 +8,27 @@ class_name Unit
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var health_bar: SegmentedProgressBar = $HealthBar
 @onready var health_component: HealthComponent = $HealthComponent
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
 var _card: CardDefinition
 var _is_enemy: bool
 
 
 func _ready():
+	area_entered.connect(_on_area_entered)
 	ShotTimer.timeout.connect(_on_ShotTimer_timeout)
 	ShotTimer.start()
+
+
+func _on_area_entered(_area):
+	print('entered')
 
 
 func _on_ShotTimer_timeout():
 	var projectile = Projectile.instantiate()
 	projectile.direction.y = 1 if _is_enemy else -1
 	add_child(projectile)
+	projectile.init(_is_enemy)
 
 	animation_player.play("recoil")
 
@@ -40,3 +47,8 @@ func init(card: CardDefinition, pos: Vector2, is_enemy: bool):
 	health_component.health = card.health
 	health_bar.max_value = card.health
 	health_bar.value = card.health
+
+	var collision_layer = Constants.ENEMY_UNIT_LAYER if is_enemy else Constants.PLAYER_UNIT_LAYER
+	var mask_layer = Constants.ENEMY_PROJECTILE_LAYER if is_enemy else Constants.PLAYER_PROJECTILE_LAYER
+	set_collision_layer_value(collision_layer, true)
+	set_collision_mask_value(mask_layer, true)
